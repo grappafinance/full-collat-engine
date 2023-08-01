77e3a254844de5dee096e85886b201d1405b0582
Anton Cheng <antonassocareer@gmail.com>
2023-08-01 23:48:07 +0800

    chore: minimize gas impact

--- certora/harness/FullMarginEngineExtended.sol
+++ certora/harness/FullMarginEngineExtended.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.0;
 
 import {FullMarginEngine} from "src/FullMarginEngine.sol";
 import {FullMarginLib} from "src/FullMarginLib.sol";
+import {TokenIdUtil} from  "grappa-core/libraries/TokenIdUtil.sol";
+import {ProductIdUtil} from "grappa-core/libraries/ProductIdUtil.sol";
+
+import {TokenType} from  "grappa-core/config/enums.sol";
 
 /**
  * @title FullMarginEngineExtended
@@ -12,6 +16,8 @@ contract FullMarginEngineExtended is FullMarginEngine {
   constructor(address _grappa, address _optionToken) FullMarginEngine(_grappa, _optionToken) {}
 
   function checkTokenIdToMint(uint256 tokenId) external view {
-    return FullMarginLib.checkTokenIdToMint(tokenId);
+    (TokenType optionType, uint40 productId,,,) = TokenIdUtil.parseTokenId(tokenId);
+    (,, uint8 underlyingId, uint8 strikeId, uint8 collateralId) = ProductIdUtil.parseProductId(productId);
+    FullMarginLib.checkProductToMint(optionType, underlyingId, strikeId, collateralId);
   }
 }
